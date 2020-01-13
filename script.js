88b2fd72a04095270180f1777a1341e8e6197927
Kailing Xing <xing0020@algonquinlive.com>
2020-01-13 15:46:25 -0500

    Add index and script to  the initial file

---
+++ script.js
@@ -0,0 +1,11 @@
+let name = 'Kailing Xing'
+console.log(`Hello, my name is ${name}.`)
+
+let number = 5
+let product = number * 2
+
+console.log(`The number ${number} multiplied by 2 is ${number * 2}.`)
+console.log(`The number mutiplied by 5 is ${product * 5}.`)
+console.log(`The result divided by 5 is ${(product * 5) / number}`)
+let finalValue = (product * 5) / number
+console.log(`The result minus 7 is ${finalValue - 7}.`)
